a35972201d1c4840015cdff6b0220ebcd6c50b2d
Surya <surajmakwana1712@gmail.com>
2025-09-08 13:40:20 +0530

    typo mistake of getUserChannelProfile function : requestAnimationFrame to req or update logoutUser to use $unset for refreshToken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -170,8 +170,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1, // remove the field from doc
       },
     },
     {
@@ -335,77 +335,75 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "Cover image updated successfully"));
 });
 
-const getUserChannelProfile = asyncHandler(
-  async (requestAnimationFrame, res) => {
-    const { username } = req.params;
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
 
-    if (!username?.trim()) {
-      throw new ApiError(400, "Username is missing");
-    }
-    const channel = await User.aggregate([
-      {
-        $match: {
-          username: username?.toLowerCase(),
-        },
-      },
-      {
-        $lookup: {
-          from: "subscriptions",
-          localField: "_id",
-          foreignField: "channel",
-          as: "subscribers",
-        },
-      },
-      {
-        $lookup: {
-          from: "subscriptions",
-          localField: "_id",
-          foreignField: "subscribers",
-          as: "subscribedTo",
-        },
-      },
-      {
-        $addFields: {
-          subscribersCount: {
-            $size: "$subscribers",
-          },
-          channelsSubscribedToCount: {
-            $size: "$subscribedTo",
-          },
-          isSubscribed: {
-            $cond: {
-              if: { $in: [req.user?._id, "$subscribers.subscribers"] },
-              then: true,
-              else: false,
-            },
-          },
-        },
-      },
-      {
-        $project: {
-          fullName: 1,
-          username: 1,
-          subscribersCount: 1,
-          channelsSubscribedToCount: 1,
-          isSubscribed: 1,
-          avatar: 1,
-          coverImage: 1,
-          email: 1,
-        },
-      },
-    ]);
-
-    if (!channel?.length) {
-      throw new ApiError(404, "Channel does not exists");
-    }
-
-    return res
-      .status(200)
-      .json(
-        new ApiResponse(200, channel[0], "User channel fetched successfully")
-      );
+  if (!username?.trim()) {
+    throw new ApiError(400, "Username is missing");
   }
-);
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscribers",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+        channelsSubscribedToCount: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscribers"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      },
+    },
+  ]);
+
+  if (!channel?.length) {
+    throw new ApiError(404, "Channel does not exists");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channel[0], "User channel fetched successfully")
+    );
+});
 
 const getWatchHistory = asyncHandler(async (req, res) => {
   const user = await User.aggregate([
